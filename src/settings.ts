c3d68e49c253dee2bf4a601735e740e5f81272fc
johnny <shawndotty@gmail.com>
2025-07-05 10:46:18 +0800

    when add new source, put it at first

--- src/settings.ts
+++ src/settings.ts
@@ -42,7 +42,7 @@ class FetchSourceSettingsTab extends PluginSettingTab {
 					.setButtonText(t("+ Add New Fetch Source"))
 					.setCta()
 					.onClick(() => {
-						this.plugin.settings.fetchSources.push({
+						this.plugin.settings.fetchSources.unshift({
 							name: "",
 							url: "",
 							appID: "",
